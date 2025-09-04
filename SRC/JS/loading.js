e62850af1959b2f903679fdb1fc531a8543a9efd
Ja'Quis <jaquismay2004@gmail.com>
2025-09-04 17:34:07 -0400

    Whole new site

--- SRC/JS/loading.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function () {
-  const loadingScreen = document.getElementById("loading-screen");
-  const content = document.getElementById("content");
-
-  loadingScreen.style.display = "none";
-});
---
+++ src/js/index.js
@@ -0,0 +1,61 @@
+function appear(newSrc) {
+  document.getElementById("sampler-closed").src = newSrc;
+}
+
+function close() {
+  document.getElementById("sampler-closed").src =
+    "/src/assets/Images/Closed-Plate.png";
+}
+
+function ribappear(newSrc) {
+  document.getElementById("ribplate-closed").src = newSrc;
+}
+
+function ribclose() {
+  document.getElementById("ribplate-closed").src =
+    "/src/assets/Images/cls-plate.png";
+}
+
+const samplerImage = document.getElementById("sampler-closed");
+
+const originalSrc = "/src/assets/Images/Cls-plate.png";
+
+const hoverSrc = "/src/assets/Images/Sampler.png";
+
+samplerImage.addEventListener("mouseover", function () {
+  this.src = hoverSrc;
+});
+
+samplerImage.addEventListener("mouseout", function () {
+  this.src = originalSrc;
+});
+
+const ribImage = document.getElementById("ribplate-closed");
+
+const ogRib = "/src/assets/Images/Rib-Platwe";
+
+function handleRibplateImage() {
+  const ribplate = document.getElementById("ribplate-closed");
+  if (window.innerWidth < 768) {
+    ribplate.src = "/src/assets/Images/Rib-Plate.png";
+  } else {
+    ribplate.src = "/src/assets/Images/Rib-Plate.png";
+  }
+}
+
+handleRibplateImage();
+
+window.addEventListener("resize", handleRibplateImage);
+
+function handleSamplerImage() {
+  const ribplate = document.getElementById("sampler-closed");
+  if (window.innerWidth < 768) {
+    ribplate.src = "/src/assets/Images/Sampler.png";
+  } else {
+    ribplate.src = "/src/assets/Images/Sampler.png";
+  }
+}
+
+handleSamplerImage();
+
+window.addEventListener("resize", handleRibplateImage);
