fbc2100cee6c3058fbcb5763a5d0d6938ee31619
Ja'Quis <jaquismay2004@gmail.com>
2025-09-05 18:09:01 -0400

    webp lol

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ function appear(newSrc) {
 
 function close() {
   document.getElementById("sampler-closed").src =
-    "/src/assets/Images/Closed-Plate.png";
+    "/src/assets/Images/Cls-plate.webp";
 }
 
 function ribappear(newSrc) {
@@ -13,14 +13,14 @@ function ribappear(newSrc) {
 
 function ribclose() {
   document.getElementById("ribplate-closed").src =
-    "/src/assets/Images/cls-plate.png";
+    "/src/assets/Images/cls-plate.webp";
 }
 
 const samplerImage = document.getElementById("sampler-closed");
 
-const originalSrc = "/src/assets/Images/Cls-plate.png";
+const originalSrc = "/src/assets/Images/Cls-plate.webp";
 
-const hoverSrc = "/src/assets/Images/Sampler.png";
+const hoverSrc = "/src/assets/Images/Sampler.webp";
 
 samplerImage.addEventListener("mouseover", function () {
   this.src = hoverSrc;
@@ -37,9 +37,9 @@ const ogRib = "/src/assets/Images/Rib-Platwe";
 function handleRibplateImage() {
   const ribplate = document.getElementById("ribplate-closed");
   if (window.innerWidth < 768) {
-    ribplate.src = "/src/assets/Images/Rib-Plate.png";
+    ribplate.src = "/src/assets/Images/Rib-Plate.webp";
   } else {
-    ribplate.src = "/src/assets/Images/Rib-Plate.png";
+    ribplate.src = "/src/assets/Images/Rib-Plate.webp";
   }
 }
 
@@ -50,9 +50,9 @@ window.addEventListener("resize", handleRibplateImage);
 function handleSamplerImage() {
   const ribplate = document.getElementById("sampler-closed");
   if (window.innerWidth < 768) {
-    ribplate.src = "/src/assets/Images/Sampler.png";
+    ribplate.src = "/src/assets/Images/Sampler.webp";
   } else {
-    ribplate.src = "/src/assets/Images/Sampler.png";
+    ribplate.src = "/src/assets/Images/Sampler.webp";
   }
 }
 
@@ -64,6 +64,6 @@ function serveLogo()
 {
   if (window.outerWidth() < 768)
   {
-    document.getElementById("logo-animation-main").src = "/SRC/ASSETS/IMAGES/Maydayz-LOGO-TailGate-GS.png"
+    document.getElementById("logo-animation-main").src = "/SRC/ASSETS/IMAGES/Maydayz-LOGO-TailGate-GS.webp"
   }
 }
