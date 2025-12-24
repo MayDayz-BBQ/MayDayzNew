0507fb48a73ee7e0ef1ed7283fd5b9f518558feb
Ja'Quis <soxietymarketing@gmail.com>
2025-12-23 19:58:00 -0500

    Fixing these logs

--- SRC/JS/clerk-auth.js
+++ SRC/JS/clerk-auth.js
@@ -1,8 +1,5 @@
-// This function will run once the document is fully loaded
 window.addEventListener("load", async () => {
-  // Get your publishable key from your Clerk environment variables
   const clerkPubKey = "pk_live_Y2xlcmsubWF5ZGF5ei5jb20k";
-
   const clerk = window.Clerk;
 
   if (!clerk) {
@@ -11,43 +8,36 @@ window.addEventListener("load", async () => {
   }
 
   try {
-    await clerk.load({
-      // You can add options here if needed, like afterSignInUrl, etc.
-    });
+    await clerk.load({});
 
-    //Sign in within login.html
     const signInDiv = document.getElementById("sign-in");
-
     const userProfileDiv = document.getElementById("user-profile");
-    //Sign in for the index page
     const userSignInDiv = document.getElementById("Sign-up");
     const userSignOutDiv = document.getElementById("Sign-out");
 
     if (clerk.user) {
       if (signInDiv) signInDiv.style.display = "none";
-      if (userProfileDiv) userProfileDiv.style.display = "block";
       if (userProfileDiv) {
+        userProfileDiv.style.display = "block";
         clerk.mountUserProfile(userProfileDiv, {
           afterSignOutUrl: "/Index.html",
         });
       }
-      if (userSignOutDiv) userSignOutDiv.style.display = "flex";
       if (userSignOutDiv) {
+        userSignOutDiv.style.display = "flex";
         clerk.mountUserButton(userSignOutDiv, {
           afterSignOutUrl: "/Index.html",
         });
       }
       if (userSignInDiv) userSignInDiv.style.display = "none";
-      if (userSignInDiv) document.getElementById("");
-      if (userSignInDiv) {
-        clerk.mountUserButton(userSignInDiv, {
-          afterSignOutUrl: "/Index.html",
-        });
-      }
     } else {
-      signInDiv.style.display = "block";
-      userProfileDiv.style.display = "none";
-      clerk.mountSignIn(signInDiv);
+      if (signInDiv) {
+        signInDiv.style.display = "block";
+        clerk.mountSignIn(signInDiv);
+      }
+      if (userProfileDiv) {
+        userProfileDiv.style.display = "none";
+      }
     }
   } catch (err) {
     console.error("Failed to load ClerkJS:", err);
