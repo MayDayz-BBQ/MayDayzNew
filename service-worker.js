1a11b4b92a062038f669e324cf423917852fa6f9
Ja'Quis <jaquismay2004@gmail.com>
2025-12-31 21:07:07 -0500

    Changing the worker a little

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,20 @@
-self.addEventListener("install", (event) => {
-    console.log("[SW] Installing self-destructing Service Worker...");
+  self.addEventListener("install", (event) => {
     event.waitUntil(self.skipWaiting());
   });
   
   self.addEventListener("activate", (event) => {
-    console.log("[SW] Activating self-destructing Service Worker...");
     event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          // Delete ALL caches controlled by this origin
-          return Promise.all(
-            cacheNames.map((cacheName) => {
-              console.log(`[SW] Deleting cache: ${cacheName}`);
-              return caches.delete(cacheName);
-            })
-          );
-        })
-        .then(() => {
-          console.log("[SW] All caches cleared. Unregistering self.");
-          // Get all clients controlled by this Service Worker
-          return self.clients.claim().then(() => {
-            // Find the controlling Service Worker and unregister it
-            return self.registration.unregister();
-          });
-        })
-        .then(() => {
-          console.log(
-            "[SW] Service Worker successfully unregistered. Please refresh the page."
-          );
-        })
-        .catch((error) => {
-          console.error("[SW] Error during unregistration:", error);
-        })
+      caches.keys().then((keys) => {
+        return Promise.all(keys.map(key => caches.delete(key)));
+      }).then(() => {
+        return self.registration.unregister();
+      }).then(() => {
+        console.log("SW Unregistered");
+      })
     );
   });
   
+  self.addEventListener("fetch", (event) => {
+    // console.log('[SW] Fetch event - passing through for unregistering SW:', event.request.url);
+    return;
+  });
